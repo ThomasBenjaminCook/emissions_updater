7cd54c8ccc87b69356face22e28a8cf2a5a5c7cb
Tom <123046600+ThomasBenjaminCook@users.noreply.github.com>
2023-06-26 14:06:43 +1000

    able to send csv file to database in python anywhere

---
+++ update_database.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import pandas as pd
+from sqlalchemy import create_engine
+
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="ThomasAppMaker",
+    password="P_R5nvjG5DV4Vd6",
+    hostname="ThomasAppMaker.mysql.pythonanywhere-services.com",
+    databasename="ThomasAppMaker$ipcollect",
+)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+THIS_FOLDER = Path(__file__).parent.resolve()
+
+df = pd.read_csv(THIS_FOLDER / "emissions_file.csv")
+df.to_sql("User", con=engine, if_exists="replace")
