9d1b8bbbbd132d436c56985733230f7b7f30511a
Tom <123046600+ThomasBenjaminCook@users.noreply.github.com>
2023-06-26 14:34:26 +1000

    I broke the database so now I need to put it back

--- update_database.py
+++ update_database.py
@@ -12,8 +12,8 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 THIS_FOLDER = Path(__file__).parent.resolve()
 
-df = pd.read_sql_table("emissions_database", con=engine, index_col=None)
+df = pd.read_csv("emissions_file.csv")
 
-print(df.head())
+#df = pd.read_sql_table("emissions_database", con=engine, index_col=None)
 
-df.set_index('level_0').to_sql("emissions_database", con=engine, if_exists="replace")
+df.to_sql("emissions_database", con=engine, if_exists="replace")
