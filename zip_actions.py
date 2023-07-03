d21bee3fa1b9d18dc0d4367dec18d8ffd8f704b5
Tom <123046600+ThomasBenjaminCook@users.noreply.github.com>
2023-07-03 14:27:44 +1000

    Properly finding the target directory of the zips

--- zip_actions.py
+++ zip_actions.py
@@ -10,7 +10,7 @@ def extract_all(extract_from, extract_to):
     file_names = get_names(extract_from)
 
     for file_name in file_names:
-        zip_file_path = os.path.join(script_directory, (extract_from + "\\" + file_name)) #Find the path to the target zip file.
+        zip_file_path = os.path.join(script_directory, (extract_from + "/" + file_name)) #Find the path to the target zip file.
         destination_folder = os.path.join(script_directory, extract_to)
 
         with zipfile.ZipFile(zip_file_path, 'r') as zip_ref: #Extract the zip file.
